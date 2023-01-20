24e9020620bbe5bf68fadccadd6319d22477177a
srihaari <srihari6032@gmail.com>
2023-01-20 15:37:45 +0530

    Learned about views and partials and view engine

--- app.js
+++ app.js
@@ -2,21 +2,36 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  //   res.send("<p>Home Page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a bew Blog" });
 });
 
 app.use((req, res) => {
-  res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
